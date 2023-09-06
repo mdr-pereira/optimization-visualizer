874ecd23b2960d8a6257deeda93696ece9c9b77d
Mawuko <mawukotettey@gmail.com>
2023-09-06 13:29:29 +0200

    all functions separated to their files

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+import function_preprocess
+import optimizer_interface
+import solution_preparation
+
+
 """
 This component will serve as the main I/O point, we want to call all stdin/out/err from here, while also trying to setup the file reads/writes from here too.
 
@@ -7,9 +12,19 @@ It also serves as the main entry point into the program, which will contain the 
 def main_loop():
 
     landscape_eq_str = input("Enter an equation with parameter x. (e.g., 2*x**2 + 3*x - 5): ")
+    optimizer_choice = input("Select an Optimizer by the number: 1.Hill-Climber ")
+    function = function_preprocess.process_function_str(landscape_eq_str)
+    values = function_preprocess.generate_datapoints(function)
+
+    if optimizer_choice == 1:
+        res_i, res_data = optimizer_interface.MyOptimizer.simple_walk(values)
+    else:
+        pass
 
 
-    print("A")
+
+
+
 
 if __name__=="__main__":
     main_loop()
--- src/visualization.py
+++ src/visualization.py
@@ -1,3 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 """
 We want to write all of the visualization functions in here, receiving the data from the main loop over at the "main.py" file.
 """
+
+
+def anim_func(f, data_i, data, line: plt.Line2D):
+    line.set_data((data_i[:f],  data[:f]))
+
+Writer = animation.writers['ffmpeg']
+Writer = Writer(fps=10, metadata=dict(artist="Me"), bitrate=-1)
+
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(data)
+ax2 = ax.twinx()
+l, = ax.plot([], [], 'r-')
+anim = animation.FuncAnimation(fig, anim_func, interval=50, fargs=(res_i, res_data, l), save_count=len(res_i))
+anim.save("lines.mp4", writer=Writer)
